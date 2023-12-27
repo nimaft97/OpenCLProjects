39a375d69030bef63877730656e001b59e714096
nimaft97 <nimafatholahi@gmail.com>
2023-12-27 12:20:06 -0800

    changed the format from cl to clh for kernels, use vector instead of C-style array in Data.h and added more initialization to prefixScan.cpp so that the only step left is implementation of the kernels

--- prefix-scan/PrefixScan.cpp
+++ prefix-scan/PrefixScan.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
 #include "include/Data.h"
 #include "include/common.h"
+#include <string>
 // OpenCL includes
 #include <CL/cl.h>
 
 int main()
 {
-    // read data
-    int* host_data = data;
-
     // initialize OpenCL
     cl_int err = CL_SUCCESS;
     cl_platform_id platform_id;
@@ -39,22 +37,59 @@ int main()
     CHECK_CL_ERROR(err, "Couldn't create the queue");
 
     // create a program from kernel source code
-    const auto file_name = "kernels.cl";
-    const char* kernel_source  = readFile("kernels.cl").c_str();
+    const auto file_name = "kernels.clh";
+    const auto kernel_source_string = readFile(file_name);
+    const char* kernel_source = kernel_source_string.c_str();
     program = clCreateProgramWithSource(context, 1, &kernel_source, NULL, &err);
     CHECK_CL_ERROR(err, "Couldn't create the program");
     // build the program
     clBuildProgram(program, 1, &device, NULL, NULL, NULL);
+    
+    // read data
+    std::vector<int> host_data = data;  // copy
+    const size_t length = host_data.size();
+    const size_t size_in_byte = length * sizeof(decltype(host_data.at(0)));
+    // create buffer(s)
+    cl_mem device_data = clCreateBuffer(context, CL_MEM_READ_WRITE, size_in_byte, NULL, &err);
+    CHECK_CL_ERROR(err, "Couldn't create the buffer on the GPU");
+    
+    // transfer data to GPU
+    clEnqueueWriteBuffer(queue, device_data, CL_TRUE, 0, size_in_byte, host_data.data(), 0, NULL, NULL);
 
     // build kernel(s)
     cl_kernel kernel_prefix_sum = clCreateKernel(program, "prefixSum", &err);
     CHECK_CL_ERROR(err, "Couldn't create the prefixSum kernel");
 
-    // code to be added
+    // set kernel args
+    err = clSetKernelArg(kernel_prefix_sum, 0, sizeof(cl_mem), &device_data);
+    CHECK_CL_ERROR(err, "Couldn't set arg 1");
+    clSetKernelArg(kernel_prefix_sum, 1, sizeof(length), &length);
+    CHECK_CL_ERROR(err, "Couldn't set arg 2");
+
+    // set global and local sizes (grid and block sizes)
+    size_t global_size = 256u;
+    size_t local_size = 256u;
+
+    // enqueue the kernel for execution
+    err = clEnqueueNDRangeKernel(queue, kernel_prefix_sum, 1, NULL, &global_size, &local_size, 0, NULL, NULL);
+    CHECK_CL_ERROR(err, "Couldn't launch the prefixSum kernel");
+
+    // wait until execution of the kernel is over
+    err = clFinish(queue);
+
+    // read the kernel's output
+    clEnqueueReadBuffer(queue, device_data, CL_TRUE, 0, size_in_byte, host_data.data(), 0, NULL, NULL);
 
     // Release resources
+    clReleaseMemObject(device_data);
     clReleaseKernel(kernel_prefix_sum);
     clReleaseProgram(program);
     clReleaseContext(context);
     
+
+    // for (auto i=0u; i<length; ++i)
+    // {
+    //     std::cout << host_data[i] << " ";
+    // }
+    std::cout << std::endl;
 }
